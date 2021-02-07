5c18c33b389fed660a47a9e6d698dea118506bbd
YashP88 <74104184+YashP88@users.noreply.github.com>
2021-02-07 15:14:34 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+
+var backGroundImage;
+var ground;
+var hero;
+var fly;
+
+
+function preload() {
+//preload the images here
+backGroundImage=loadImage("GamingBackground.png");
+
+}
+
+function setup() {
+  createCanvas(3000, 800);
+  // create sprites here
+  engine = Engine.create();
+	world = engine.world;
+  
+  ground=new Ground(1000,700,3000,20);
+  hero=new Hero(150,200,80);
+  fly=new Fly(hero.body,{x:220,y:200});
+      
+  Engine.run(engine);
+}
+
+function draw() {
+  background(backGroundImage);
+
+  ground.display();
+  hero.display();
+  
+
+}
+
+function mouseDragged(){
+  Matter.Body.setPosition(hero.body, {x: mouseX , y: mouseY});
+}
+
+function mouseReleased(){
+  fly.flya();
+}
+
+function keyPressed(){
+  if(keyCode===32){
+    Matter.Body.setPosition(hero.body,{x:150,y:200})
+    fly.attach(hero.body);
+  }
+}
+	
+  
